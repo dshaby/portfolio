148fa70598fe8936ca2979cd3accae5d7d1a8bb2
Daniel Shaby <dshaby@berkeley.edu>
2024-12-17 16:21:06 -0600

    preloads profile image, adds loading indicator, & converts About to Functional Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,95 +1,118 @@
-import React, { Component } from 'react';
-import { Icon } from '@iconify/react';
-import nodejsIcon from '@iconify/icons-logos/nodejs-icon';
-import reactIcon from '@iconify/icons-logos/react';
-import typescriptIcon from '@iconify/icons-logos/typescript-icon';
+import React, { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
+import nodejsIcon from "@iconify/icons-logos/nodejs-icon";
+import reactIcon from "@iconify/icons-logos/react";
+import typescriptIcon from "@iconify/icons-logos/typescript-icon";
 
-class About extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var profilepic = 'images/' + this.props.sharedBasicInfo.image;
+function About(props) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  if (props?.sharedBasicInfo) {
+    var profilepic = "images/" + props.sharedBasicInfo.image;
+  }
+  if (props?.resumeBasicInfo) {
+    var sectionName = props.resumeBasicInfo.section_name.about;
+    var hello = props.resumeBasicInfo.description_header;
+    var about = props.resumeBasicInfo.description;
+  }
+
+  useEffect(() => {
+    if (profilepic) {
+      const img = new Image();
+      img.src = profilepic;
+      img.onload = () => {
+        setImageLoaded(true);
+      };
     }
-    if (this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.about;
-      var hello = this.props.resumeBasicInfo.description_header;
-      var about = this.props.resumeBasicInfo.description;
-    }
+  }, [profilepic]);
 
-    return (
-      <section id="about">
-        <div className="col-md-12">
-          <h1 style={{ color: 'black' }}>
-            <span>{sectionName}</span>
-          </h1>
-          <div className="row center mx-auto mb-5">
-            <div className="col-md-4 mb-5 center">
-              <div className="polaroid">
-                <span style={{ cursor: 'auto' }}>
-                  <img
-                    height="350px"
-                    src={profilepic}
-                    alt="Avatar placeholder"
-                  />
-                  <Icon
-                    icon={reactIcon}
-                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                  />
-                  <Icon
-                    icon={typescriptIcon}
-                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                  />
-                  <Icon
-                    icon={nodejsIcon}
-                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
-                  />
-                </span>
-              </div>
-            </div>
-
-            <div className="col-md-8 center">
-              <div className="col-md-10">
-                <div className="card">
-                  <div className="card-header">
-                    <span
-                      className="iconify"
-                      data-icon="emojione:red-circle"
-                      data-inline="false"
-                    ></span>{' '}
-                    &nbsp;{' '}
-                    <span
-                      className="iconify"
-                      data-icon="twemoji:yellow-circle"
-                      data-inline="false"
-                    ></span>{' '}
-                    &nbsp;{' '}
-                    <span
-                      className="iconify"
-                      data-icon="twemoji:green-circle"
-                      data-inline="false"
-                    ></span>
-                  </div>
+  return (
+    <section id="about">
+      <div className="col-md-12">
+        <h1 style={{ color: "black" }}>
+          <span>{sectionName}</span>
+        </h1>
+        <div className="row center mx-auto mb-5">
+          <div className="col-md-4 mb-5 center">
+            <div className="polaroid">
+              <span style={{ cursor: "auto" }}>
+                {imageLoaded ? (
+                  <img height="350px" src={profilepic} alt="Daniel Shaby" />
+                ) : (
                   <div
-                    className="card-body font-trebuchet text-justify ml-3 mr-3"
                     style={{
-                      height: 'auto',
-                      fontSize: '132%',
-                      lineHeight: '200%',
+                      height: "350px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
                     }}
                   >
-                    <br />
-                    <span className="wave">{hello} :) </span>
-                    <br />
-                    <br />
-                    {about}
+                    <div
+                      className="spinner-border text-primary"
+                      role="status"
+                    ></div>
                   </div>
+                )}
+
+                <Icon
+                  icon={reactIcon}
+                  style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                />
+                <Icon
+                  icon={typescriptIcon}
+                  style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                />
+                <Icon
+                  icon={nodejsIcon}
+                  style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                />
+              </span>
+            </div>
+          </div>
+
+          <div className="col-md-8 center">
+            <div className="col-md-10">
+              <div className="card">
+                <div className="card-header">
+                  <span
+                    className="iconify"
+                    data-icon="emojione:red-circle"
+                    data-inline="false"
+                  ></span>{" "}
+                  &nbsp;{" "}
+                  <span
+                    className="iconify"
+                    data-icon="twemoji:yellow-circle"
+                    data-inline="false"
+                  ></span>{" "}
+                  &nbsp;{" "}
+                  <span
+                    className="iconify"
+                    data-icon="twemoji:green-circle"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div
+                  className="card-body font-trebuchet text-justify ml-3 mr-3"
+                  style={{
+                    height: "auto",
+                    fontSize: "132%",
+                    lineHeight: "200%",
+                  }}
+                >
+                  <br />
+                  <span className="wave">{hello} :) </span>
+                  <br />
+                  <br />
+                  {about}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default About;
